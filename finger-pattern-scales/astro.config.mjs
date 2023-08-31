7b9bcf177c5aa83cf31211ceb63c842d18877d5c
Rick Henry <rickhenry@rickhenry.dev>
2023-08-31 19:40:27 -0400

    switch to fully static deployment
    Signed-off-by: Rick Henry <rickhenry@rickhenry.dev>

--- finger-pattern-scales/astro.config.mjs
+++ finger-pattern-scales/astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import preact from "@astrojs/preact";
-import vercel from "@astrojs/vercel/serverless";
 
 import sitemap from "@astrojs/sitemap";
 
@@ -9,6 +8,5 @@ import sitemap from "@astrojs/sitemap";
 export default defineConfig({
   site: "https://finger-patterns.rickhenry.studio",
   integrations: [tailwind(), preact(), sitemap()],
-  output: "hybrid",
-  adapter: vercel()
+  output: "static",
 });
