f1757d55345d9cf48ffe037beacc2499840994b5
Rick Henry <rickhenry@rickhenry.dev>
2023-09-03 18:56:46 -0400

    create completion code
    Signed-off-by: Rick Henry <rickhenry@rickhenry.dev>

---
+++ finger-pattern-scales/api/feedback/go.mod
@@ -0,0 +1,5 @@
+module github.com/rickh94/finger-patterns-with-quiz/api/feedback
+
+go 1.20
+
+require github.com/sashabaranov/go-openai v1.15.1 // indirect
--- finger-pattern-scales/api/feedback/index.go
+++ finger-pattern-scales/api/feedback/index.go
@@ -1,9 +1,13 @@
 package feedback
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"os"
+
+	openai "github.com/sashabaranov/go-openai"
 )
 
 type Missed struct {
@@ -21,10 +25,61 @@ type QuizResults struct {
 func Handler(w http.ResponseWriter, r *http.Request) {
 	var quizResults QuizResults
 
-	var data = json.NewDecoder(r.Body)
-	if err := data.Decode(&quizResults); err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
+	if err := json.NewDecoder(r.Body).Decode(&quizResults); err != nil {
+		http.Error(w, "Could not decode Results Json", http.StatusBadRequest)
 		return
 	}
-	fmt.Fprintf(w, "Correct: %d, Incorrect: %d", quizResults.Correct, quizResults.Incorrect)
+	apiKey := os.Getenv("OPENAI_KEY")
+	if apiKey == "" {
+		http.Error(w, "No OpenAI Key", http.StatusInternalServerError)
+		return
+	}
+	var violinStrings []string
+	for violinString, count := range quizResults.Missed.ViolinString {
+		if count >= 2 {
+			violinStrings = append(violinStrings, violinString)
+		}
+	}
+
+	var patterns []string
+	for pattern, count := range quizResults.Missed.PatternId {
+		if count >= 2 {
+			switch pattern {
+			case "oneTwo":
+				patterns = append(violinStrings, "1-2 Pattern")
+			case "twoThree":
+				patterns = append(violinStrings, "2-3 Pattern")
+			case "threeFour":
+				patterns = append(violinStrings, "3-4 Pattern")
+			case "wholeSteps":
+				patterns = append(violinStrings, "Whole Steps Pattern")
+			case "halfSteps":
+				patterns = append(violinStrings, "Half Steps Pattern")
+			default:
+				continue
+			}
+		}
+	}
+
+	client := openai.NewClient(apiKey)
+
+	percentage := quizResults.Correct / (quizResults.Correct + quizResults.Incorrect) * 100
+	prompt := fmt.Sprintf("Write a positive tone message giving feedback base on a quiz score of %v percent"+
+		" then tell the user they need to practice more on the following, if provided: "+
+		" Strings: %v, Patterns: %v.", percentage, violinStrings, patterns)
+
+	req := openai.CompletionRequest{
+		Model:     openai.GPT3Ada,
+		MaxTokens: 200,
+		Prompt:    prompt,
+	}
+
+	ctx := context.Background()
+	resp, err := client.CreateCompletion(ctx, req)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	// fmt.Fprintf(w, "Correct: %d, Incorrect: %d", quizResults.Correct, quizResults.Incorrect)
+	fmt.Fprintf(w, resp.Choices[0].Text)
 }
