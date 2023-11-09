0b8f9778a26889647a7669a1cb76467ed29e6825
Rick Henry <rickhenry@rickhenry.dev>
2023-11-09 13:42:05 -0500

    add vercel analytics
    Signed-off-by: Rick Henry <rickhenry@rickhenry.dev>

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import preact from '@astrojs/preact';
+import vercel from '@astrojs/vercel/serverless';
 
 import sitemap from '@astrojs/sitemap';
 
@@ -16,5 +17,10 @@ export default defineConfig({
         'https://violinfingerpatterns.com/all-the-patterns/all-of-them/',
     }),
   ],
-  output: 'static',
+  output: 'hybrid',
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
 });
